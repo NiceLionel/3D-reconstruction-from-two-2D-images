ef71e23da0cff860031a483c2175b67ad7c59257
NiceLionel <90742138+NiceLionel@users.noreply.github.com>
2023-09-21 18:58:58 -0400

    Update pose.py

--- code/pose.py
+++ code/pose.py
@@ -3,7 +3,6 @@ import numpy as np
 def pose_candidates_from_E(E):
   transform_candidates = []
   ##Note: each candidate in the above list should be a dictionary with keys "T", "R"
-  """ YOUR CODE HERE
   """
   Rz1 = np.array([[0, -1, 0],
                   [1, 0, 0],
@@ -20,6 +19,5 @@ def pose_candidates_from_E(E):
   transform_candidates.append({"T":T1, "R":R2})
   transform_candidates.append({"T":T2, "R":R1})
   transform_candidates.append({"T":T2, "R":R2})
-  """ END YOUR CODE
   """
   return transform_candidates
